7f7326ecca02b4c881be8f8a9c564b8c5a53f0a2
revan <gujarevan@gmail.com>
2023-01-15 02:54:40 +0530

    Initial commmit

---
+++ src/main/java/in/revan/reponamechange/ReponameChangeApplication.java
@@ -0,0 +1,13 @@
+package in.revan.reponamechange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReponameChangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReponameChangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/revan/reponamechange/ReponameChangeApplicationTests.java
@@ -0,0 +1,13 @@
+package in.revan.reponamechange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReponameChangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
